023a68402410fad12cd951d503cb90f4b0506b95
Kevin Lee <kevin.code@kevinlee.io>
2020-06-02 23:25:25 +1000

    Added: SbtLogger for logger-f

---
+++ src/main/scala/loggerf/sbt/SbtLogger.scala
@@ -0,0 +1,18 @@
+package loggerf.sbt
+
+import sbt.Logger
+
+final class SbtLogger(val log: Logger) extends loggerf.Logger {
+
+  override def debug(message: String): Unit = log.debug(message)
+
+  override def info(message: String): Unit = log.info(message)
+
+  override def warn(message: String): Unit = log.warn(message)
+
+  override def error(message: String): Unit = log.error(message)
+}
+
+object SbtLogger {
+  def sbtLogger(implicit log: Logger): SbtLogger = new SbtLogger(log)
+}
