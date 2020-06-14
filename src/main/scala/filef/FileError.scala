c5d3d11acbeba4d101134e56fe9379a94f55f281
Kevin Lee <kevin.code@kevinlee.io>
2020-06-14 10:30:25 +1000

    Added: FileF.relativePathOf to find relative path of the file in the given base directory.

--- src/main/scala/filef/FileError.scala
+++ src/main/scala/filef/FileError.scala
@@ -1,6 +1,8 @@
 package filef
 
-import java.io.{FileNotFoundException, IOException}
+import java.io.{File, FileNotFoundException, IOException}
+
+import errors.StackTraceToString
 
 import scala.util.control.NonFatal
 
@@ -11,16 +13,19 @@ import scala.util.control.NonFatal
 sealed trait FileError
 
 object FileError {
+
   final case class FileNotFound private (
     fileNotFoundException: FileNotFoundException
   ) extends FileError
-  private[this] object FileNotFound extends (FileNotFoundException => FileError)
 
   final case class Io private (ioException: IOException) extends FileError
-  private[this] object Io extends (IOException => FileError)
 
   final case class Unknown private (throwable: Throwable) extends FileError
-  private[this] object Unknown extends (Throwable => Unknown)
+
+  final case class NotDirectory(file: File) extends FileError
+
+  final case class NotInBaseDir(baseDir: File, file: File) extends FileError
+
 
   def fromNonFatal(throwable: Throwable): FileError = throwable match {
     case fileNotFoundException: FileNotFoundException =>
@@ -40,5 +45,26 @@ object FileError {
   def io(ioException: IOException): FileError = Io(ioException)
 
   def unknown(throwable: Throwable): FileError = Unknown(throwable)
+
+  def notDirectory(file: File): FileError = NotDirectory(file)
+
+  def notInBaseDir(baseDir: File, file: File): FileError = NotInBaseDir(baseDir, file)
+
+  def render(fileError: FileError): String = fileError match {
+    case FileNotFound(fileNotFoundException) =>
+      s"No file found - Error: ${StackTraceToString.render(fileNotFoundException)}"
+
+    case Io(ioException) =>
+      s"IOException - Error: ${StackTraceToString.render(ioException)}"
+
+    case Unknown(throwable) =>
+      s"Unknown NonFatal Throwable - Error: ${StackTraceToString.render(throwable)}"
+
+    case NotDirectory(file) =>
+      s"FileError.NotDirectory - $file is not a directory."
+
+    case NotInBaseDir(baseDir, file) =>
+      s"The given file [$file] is not in the baseDir [$baseDir]"
+  }
 
 }
