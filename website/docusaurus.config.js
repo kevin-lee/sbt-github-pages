56c5add888e57981d662b4f8e9dcbc4ce51cae0d
Kevin Lee <kevin.code@kevinlee.io>
2020-07-06 02:39:26 +1000

    Changed: syntax highlighter theme

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -17,6 +17,8 @@ const websiteConfig = {
   projectName: 'sbt-github-pages', // Usually your repo name.
   themeConfig: {
     prism: {
+      theme: require('prism-react-renderer/themes/nightOwl'),
+      darkTheme: require('prism-react-renderer/themes/nightOwl'),
       additionalLanguages: ['scala'],
     },
     navbar: {
