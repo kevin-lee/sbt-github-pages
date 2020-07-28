b92d84345f6d523edcbadec998c2f71ddbc4bca3
Kevin Lee <kevin.code@kevinlee.io>
2020-07-28 23:41:38 +1000

    Prepare to release v0.1.3

--- project/ProjectInfo.scala
+++ project/ProjectInfo.scala
@@ -2,7 +2,7 @@ import wartremover.{Wart, Warts}
 
 object ProjectInfo {
 
-  val ProjectVersion: String = "0.1.2"
+  val ProjectVersion: String = "0.1.3"
 
   val commonScalacOptions: Seq[String] = Seq(
       "-deprecation"
